f0b72fa3ae0b9f8b72076e37dce4eb7b8a2b7b0a
melchor629 <melchor9000@gmail.com>
2019-12-25 20:46:08 +0100

    add copy-to-dist script

---
+++ scripts/copy-to-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf dist
+
+for package_path in packages/*; do
+  package=$(echo $package_path | cut -d/ -f2)
+  dist_path="dist/${package}"
+
+  if [ "${package}" = "home" ]; then
+    dist_path="dist/"
+  fi
+
+  if [ -d "${package_path}/dist" ]; then
+    echo "> package ${package} -> ${dist_path}"
+    mkdir -p "${dist_path}"
+    cp -r "${package_path}/dist"/* "${dist_path}"
+  fi
+done
